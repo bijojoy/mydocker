d6d618cf9e32effba013ffc8409d3a96d3b31917
Bijo Joy <bijojoy123@gmail.com>
2017-10-24 16:09:44 +0100

    first commit

---
+++ mvn_Dockerfile_build.sh
@@ -0,0 +1,3 @@
+docker build -f mvn_Dockerfile -t mvn_clean_package_3.5.0:1.1 .
+#docker tag mvn_clean_package_3.5.0:1.1 bijojoy/mvn_clean_package_3.5.0-1.1
+#docker push bijojoy/mydocker:mvn_clean_package_3.5.0-1.1
---
+++ mvn_version_Dockerfile_build.sh
@@ -0,0 +1,3 @@
+docker build -f mvn_version_Dockerfile -t mvn_version_3.5.0:1.0 .
+#docker tag mvn_version_3.5.0:1.0 bijojoy/mydocker:mvn_version_3.5.0-1.0
+#docker push bijojoy/mydocker:mvn_version_3.5.0-1.0
